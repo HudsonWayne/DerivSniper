3a1ae5d1049347a99a4ac9b1a6d57cf9391a4048
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-11 10:46:36 +0200

    asynch defination main

--- DerivSnipper/src/python_scripts/DerivSnipper.py
+++ DerivSnipper/src/python_scripts/DerivSnipper.py
@@ -9,3 +9,10 @@ SYMBOL = "R_100"
 async def main():
     api = DerivAPI(endpoint='wss://ws.derivws.com/websockets/v3', app_id=APP_ID)
     ticks = await api.ticks(SYMBOL)
+    
+    def on_update(data):
+        sym = data['instrument']
+        price = float(data['quote'])
+        epoch = int(data['epoch'])
+        sys.stdout.write(f"{sym},{epoch},{price}\n")
+        sys.stdout.flush()
