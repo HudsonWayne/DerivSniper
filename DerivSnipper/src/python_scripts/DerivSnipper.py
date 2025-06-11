60060218368c4c811b27bcf670c0f7fc3e387c13
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-11 10:45:58 +0200

    asynch defination main

--- DerivSnipper/src/python_scripts/DerivSnipper.py
+++ DerivSnipper/src/python_scripts/DerivSnipper.py
@@ -3,3 +3,9 @@ from deriv_api import DerivAPI
 
 
 APP_ID = "80707" # <--- IMPORTANT: REPLACE THIS WITH YOUR ACTUAL APP ID
+SYMBOL = "R_100"
+
+
+async def main():
+    api = DerivAPI(endpoint='wss://ws.derivws.com/websockets/v3', app_id=APP_ID)
+    ticks = await api.ticks(SYMBOL)
