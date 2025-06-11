69fbfdf7c692219f89544861e8202af4f0b607e3
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-11 10:47:29 +0200

    asynch defination main

--- DerivSnipper/src/python_scripts/DerivSnipper.py
+++ DerivSnipper/src/python_scripts/DerivSnipper.py
@@ -16,3 +16,9 @@ async def main():
         epoch = int(data['epoch'])
         sys.stdout.write(f"{sym},{epoch},{price}\n")
         sys.stdout.flush()
+        
+        ticks.on_update().subscribe(on_update)
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
