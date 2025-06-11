e464b137e7b62a1bd6d62863e16fa64fc95c4776
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-11 10:53:47 +0200

    initial commit

--- DerivSnipper/src/python_scripts/DerivSnipper.py
+++ DerivSnipper/src/python_scripts/DerivSnipper.py
@@ -1,23 +1,21 @@
 import asyncio, sys
-from deriv_api import DerivAPI
+from deriv_api import DerivAPI  # ← Correct module
 
-
-APP_ID = "80707" # <--- IMPORTANT: REPLACE THIS WITH YOUR ACTUAL APP ID
+APP_ID = "80707"
 SYMBOL = "R_100"
 
-
 async def main():
-    api = DerivAPI(endpoint='wss://ws.derivws.com/websockets/v3', app_id=APP_ID)
+    api = DerivAPI(endpoint='wss://ws.derivws.com/websockets/v3', app_id=int(APP_ID))
     ticks = await api.ticks(SYMBOL)
-    
+
     def on_update(data):
         sym = data['instrument']
         price = float(data['quote'])
         epoch = int(data['epoch'])
         sys.stdout.write(f"{sym},{epoch},{price}\n")
         sys.stdout.flush()
-        
-        ticks.on_update().subscribe(on_update)
+
+    ticks.on_update().subscribe(on_update)
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
