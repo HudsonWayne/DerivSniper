a86a9dbbf2dd07b94f31197117a32f6efc413c7b
HudsonWayne <waynewaynebenhura@gmail.com>
2025-06-11 15:19:28 +0200

    updated my project.java

--- DerivSnipper/src/main/java/com/myproject/deriv/Main.java
+++ DerivSnipper/src/main/java/com/myproject/deriv/Main.java
@@ -2,7 +2,12 @@ package com.myproject.deriv;
 
 public class Main {
     public static void main(String[] args) {
+        String appId = "YOUR_DERIV_APP_ID"; // Replace with your Deriv App ID
+        String symbol = "R_100"; // Replace with your desired volatility index symbol
+
         StrategyEngine engine = new StrategyEngine();
-        new Thread(new DerivDataReader(engine)).start();
+        DerivDataReader dataReader = new DerivDataReader(engine, appId, symbol);
+
+        new Thread(dataReader).start();
     }
 }
